2596665e267e9406534b2e8320ec1f7a76ac22d6
Heather Piwowar <hpiwowar@gmail.com>
2011-01-02 06:48:51 -0800

    update regression, still needs streamlining

--- stats/scripts/prediction.R
+++ stats/scripts/prediction.R
@@ -1,84 +1,51 @@
 dat = read.csv("../data/derived/dat_with_factor_scores.txt", header=TRUE, sep=",", stringsAsFactors=FALSE)
-dat.regress = dat
 
 library(rms)
 library(plyr)
-dd.regress = datadist(dat.regress)
-options(datadist='dd.regress')
-options(digits=2)
-
-plot(summary(wosCount ~ downloads+bookmarks+comments+wikipedia..blogs+facebook, data=dat))
-
-Glm(wosCount ~ downloads+bookmarks+comments+wikipedia..blogs+facebook, data=dat)
-
-quartz()
-plot(summary(Glm(wosCount ~ downloads+bookmarks+comments+wikipedia..blogs+facebook, data=dat)))
-
-## Include date
-
-Glm(wosCount ~ rcs(daysSincePublished,3) * (downloads+bookmarks+comments+wikipedia..blogs+facebook), data=dat)
-anova(Glm(wosCount ~ rcs(daysSincePublished,3) * (downloads+bookmarks+comments+wikipedia..blogs+facebook), data=dat))
-
-quartz()
-plot(summary(Glm(wosCount ~ rcs(daysSincePublished,3) * (downloads+bookmarks+comments+wikipedia..blogs+facebook), data=dat)))
-
-## What about without downloads
-quartz()
-plot(summary(Glm(wosCount ~ rcs(daysSincePublished,3) * (bookmarks+comments+wikipedia..blogs+facebook), data=dat)))
-
-
-### All of that is input after two years.  Look at after 6 months.
-
-dat.events.all = read.csv("../data/derived/all_2008_events.txt", header=T, sep="\t", stringsAsFactors=F)
-
-dat.eventcounts = dat
-
 library(gplots)
-library(plyr)
 
 
 #collapse into counts
-get_event_counts_month = function(dat.events.all, months, minDaysSincePublished=0, maxDaysSincePublished=100000) {
-	whereForAlts = which(dat.events.all$eventType!="wos" & dat.events.all$latency < months*30*24*60*60)
-	whereForWos = which(dat.events.all$eventType=="wos" & dat.events.all$latency < 24*30*24*60*60)
-	dat.use = dat.events.all[c(whereForAlts, whereForWos),]
+get_event_counts_month = function(dat.events, month, minDaysSincePublished=0, maxDaysSincePublished=100000) {
+	# restrict to research articles
+	dat.events = dat.events[dat.events$articleType == "Research Article", ]
+	dat.events$pubDate.y = as.POSIXct(dat.events$pubDate.y)
 
-	whereForWosShort = which(dat.events.all$eventType=="wos" & dat.events.all$latency < months*30*24*60*60)
-	dat.wos.short = dat.events.all[whereForWosShort,]
+	# restrict to those published between supplied dates
+	dat.events = dat.events[which(dat.events$daysSincePublished > minDaysSincePublished
+		& dat.events$daysSincePublished < maxDaysSincePublished),]
+
+	whereForAlts = which(dat.events$eventType!="wos" & dat.events$latency < month*30*24*60*60)
+	whereForWos = which(dat.events$eventType=="wos" & dat.events$latency < 24*30*24*60*60)
+	dat.use = dat.events[c(whereForAlts, whereForWos),]
+	dat.use$eventType[which(dat.use$eventType=="wos")] = "wos2yr"
+
+	whereForWosShort = which(dat.events$eventType=="wos" & dat.events$latency < month*30*24*60*60)
+	dat.wos.short = dat.events[whereForWosShort,]
 	dat.wos.short$eventType = "wosShort"
 	dat.use = rbind(dat.use, dat.wos.short)
 	
-	dat.events.perDoi = as.data.frame(tapply(dat.use$number.events, list(dat.use$doi, dat.use$eventType), sum))
+	dat.events.perDoi = as.data.frame(tapply(dat.use$count, list(dat.use$doi, dat.use$eventType), sum))
 	dat.events.perDoi$doi = rownames(dat.events.perDoi)
 
-	# restrict to things published in 2008
-	dat.eventcounts.merge = merge(dat.events.perDoi, dat.eventcounts, by="doi")
-	dat.eventcounts.merge.2008 = dat.eventcounts.merge["2008" == dat.eventcounts.merge$year, ]
-	dim(dat.eventcounts.merge.2008)
-
-	# restrict to PLoS ONE research articles
-	dat.eventcounts.merge.2008 = dat.eventcounts.merge.2008[dat.eventcounts.merge.2008$journal.x == "pone", ]
-	dat.eventcounts.merge.2008 = dat.eventcounts.merge.2008[dat.eventcounts.merge.2008$articleType == "Research Article", ]
-	
-	dat.eventcounts.merge.2008 = dat.eventcounts.merge.2008[which(dat.eventcounts.merge.2008$daysSincePublished > minDaysSincePublished
-		& dat.eventcounts.merge.2008$daysSincePublished < maxDaysSincePublished),]
-	# Set all the NAs to 0s  ### Not sure if this is the right thing to do...
-	dat.eventcounts.merge.2008[is.na(dat.eventcounts.merge.2008)] = 0
+	# Set all the NAs to 0s  
+	dat.events.perDoi[is.na(dat.events.perDoi)] = 0
 
 	# look at it
-	colnames(dat.eventcounts.merge.2008)
-	dat.eventcounts.merge.2008[0:5,]
-	summary(dat.eventcounts.merge.2008)
+	colnames(dat.events.perDoi)
+	dat.events.perDoi[0:5,]
+	summary(dat.events.perDoi)
 	#write.table(dat.eventcounts.merge.2008, "data/derived/merge_2008_eventcounts.txt", sep="\t", row.names=F)
-	return(dat.eventcounts.merge.2008)	
+	return(dat.events.perDoi)	
 }
 
 
-do.stuff = function (month, starting.day) {
+do.stuff = function (dat.events.merge, month, starting.day) {
 		num.days = 365
 		offset.into.2008 = (365*2+10*30) - starting.day
-		event_counts_month = get_event_counts_month(dat.events.all, month, starting.day, starting.day+num.days)
-		#subset = event_counts_month[,2:13]
+		event_counts_month = get_event_counts_month(dat.events.merge, month, starting.day, starting.day+num.days)
+		
+		event.labels = c("wosShort", "citeulike", "delicious", "backtweets", "native comments", "html views", "pdf views", "xml views", "blogs", "wos2yr")		
 		mysubset = event_counts_month[,which(names(event_counts_month) %in% event.labels)]
 		#subset.gt0 = subset[,colwise(max)(subset) > 0]
 
@@ -103,22 +70,59 @@ do.stuff = function (month, starting.day) {
 		
 		# remove html views because too correlated
 		#fit = lm(wos ~ ., data=event_counts_month[,c("daysSincePublished", "citeulike", "delicious", "backtweets", "native comments", "pdf views", "xml views", "Postgenomic via Plos", "Research Blogging via Plos", "wos")])
-		fit = lm(log(1+wos) ~ factor(journal) + num.authors + rcs(daysSincePublished, 3) * (citeulike + delicious + backtweets + log(1+`pdf views`) + log(1+`html views`) + log(1+`xml views`)), data=event_counts_month)
-		print(anova(fit))
-		print(summary(fit))
-		print(summary(fit)$r.squared)
-		print(summary(fit)$adj.r.squared)
-		return(mycor[,"wos"])
+		
+		events.month.merge = merge(event_counts_month, dat.eventcounts, by="doi")
+		
+		dd = datadist(events.month.merge[,names(events.month.merge) %nin% c("pubDate", "articleType")])
+		options(datadist="dd")
+		
+		cols = c("wos2yr", "journal", "authorsCount", "daysSincePublished", "citeulike", "delicious", "pdf views", "html views", "xml views", "blogs")
+		
+		#fit = lm(log(1+wos2yr) ~ factor(journal) + log(1+authorsCount) + daysSincePublished * (log(1+citeulike) + log(1+delicious) + log(1+backtweets) + log(1+`pdf views`) + log(1+`html views`) + log(1+`xml views`)), data=events.month.merge)
+		#fit = lm(log(1+wos2yr) ~ factor(journal) + log(1+authorsCount) + daysSincePublished * (log(1+citeulike) + log(1+delicious) + log(1+`pdf views`) + log(1+`html views`) + log(1+`xml views`)), data=events.month.merge)
+		fit = ols(log(1+wos2yr) ~ log(1+authorsCount) +  factor(journal)* log(1+`xml views`) +  (log(1+blogs) + log(1+delicious) +   log(1+`pdf views`) + log(1+`html views`) + daysSincePublished * (log(1+`xml views`)+ log(1+citeulike) )), 
+			data=events.month.merge[,cols])
+		print(fit)
+
+		fit.full = lm(log(1+wos2yr) ~ log(1+authorsCount) +  factor(journal)* log(1+`xml views`) +  (log(1+blogs) + log(1+delicious) +   log(1+`pdf views`) + log(1+`html views`) + daysSincePublished * (log(1+`xml views`)+ log(1+citeulike) )), 
+			data=events.month.merge[,cols])
+
+		fit.base = lm(log(1+wos2yr) ~ log(1+authorsCount) +  factor(journal),
+			data=events.month.merge[,cols])
+		anova(fit.full, fit.base)
+
+		#print(summary(fit))
+		#plot(anova(fit))
+		#print(anova(fit))
+		#print(summary(fit)$r.squared)
+		#print(summary(fit)$adj.r.squared)
+		return(mycor[,"wos2yr"])
 }
 
+# Note the outlier
+
+
+
+dat.events.all = read.csv("../data/derived/plos_2008_events_with_wos.txt.gz", header=T, stringsAsFactors=F)
+
+blog.labels = c("Nature via Plos", "Research Blogging via Plos", "Postgenomic via Plos")
+dat.events.all$eventType[which(dat.events.all$eventType %in% blog.labels)] = "blogs"
 
 #names(table(dat.events.all$eventType))[table(dat.events.all$eventType) > 6000]
-event.labels = c("citeulike", "delicious", "backtweets", "native comments", "html views", "pdf views", "xml views", "Postgenomic via Plos", "Research Blogging via Plos", "wos")
 
 dat.events.all$latency = as.numeric(dat.events.all$latency)
+dat.events.all$count = as.numeric(dat.events.all$count)
 
-summary(dat.eventcounts[which(dat.eventcounts$year=="2008"),]$daysSincePublished)
-days = seq(1030, 730, by=-365)
+load("../data/derived/eventcounts_preprocessed.RData")
+
+dat.events.merge = merge(dat.events.all, dat.eventcounts, by="doi")
+dat.events.merge$pubDate = dat.wosevents.merge$pubDate.x
+merged.names = c("doi", "eventType", "creator", "date", "latency", "value", "count", "pubDate", "journal", "authorsCount", "pmid", "plosSubjectTags", "plosSubSubjectTags")
+dim(dat.events.merge[,merged.names])
+
+
+#summary(dat.eventcounts[which(dat.eventcounts$year=="2008"),]$daysSincePublished)
+days = seq(730, 1030, by=365)
 i = 0
 monthChoices = c(1:5, seq(6,24, by=6))
 result = data.frame(cbind("eventType"=NULL, "month"=NULL, "starting.day"=NULL, "cor.with.wos"=NULL), stringsAsFactors=F)
@@ -127,257 +131,35 @@ for (month in monthChoices) {
 	for (starting.day in days) {
 		print(starting.day)
 		i = i+1
-		cor.with.wos = do.stuff(month, starting.day)
+		cor.with.wos = do.stuff(dat.events.merge, month, starting.day)
 		row = data.frame(eventType=names(cor.with.wos), month, starting.day, cor.with.wos, stringsAsFactors=F)
 		result = rbind(result, row)
 	}
 }
 #result
 
+
+pairs(events.month.merge[,cols])
+events.month.merge[events.month.merge$wos2yr==168,]  #10.1371/journal.pmed.0050045
+table(events.month.merge$wos2yr)
+
+
 table(result$eventType)
 sh = reshape(result, v.names=c("cor.with.wos"), timevar="eventType", idvar=c("starting.day", "month"), direction="wide")
 png(paste("../artifacts/correlations_over_time.png", sep=""), width=800, height=800)
-matplot(sh$month, sh[,3:12])
+matplot(sh$month, sh[3:12])
 dev.off()
 aggregate(sh[,3:12], by=list(sh$month), mean, na.rm=T)
 aggregate(sh[,3:12], by=list(sh$starting.day), mean, na.rm=T)
 
-quartz()
-plot(cor.with.wos ~ factor(eventType), dat=result)
-quartz()
-plot(cor.with.wos ~ starting.day, col=factor(eventType), dat=result)
-quartz()
-plot(cor.with.wos ~ month, col=factor(eventType), dat=result)
+y = predict(fit.full1, events.month.merge, interval="prediction")
+predicted = exp(y[,"fit"])-1
+actual = events.month.merge$wos2yr
+plot(predicted, actual)
 
-ee = do.stuff(3, 790)
-ff = do.stuff(3, 880)
+table(predicted>3, actual>6)
+chisq.test(table(predicted>3, actual>6))
 
-
-
-	event_counts_month = get_event_counts_month(6, 700, 790)
-	subset = event_counts_month[,2:13]
-	subset.gt0 = subset[,colwise(max)(subset) > 0]
-	names(subset.gt0)[10] = "wSh"
-	names(subset.gt0) = substr(names(subset.gt0), 1, 3)
-
-	print(month)
-		
-	m12 = glm.nb(wos ~ ., dat=subset.gt0)
-	print(summary(m12))
-	anova(m12)
-
-	m13 = glm.nb(wos ~ pdf + htm + wSh , dat=subset.gt0)
-	print(summary(m13))
-	anova(m13)
-
-	dd = datadist(subset.gt0)
-	options(datadist = "dd")
-	
-	m13 = Glm(wos ~ pdf + htm + wSh , dat=subset.gt0, 
-		family=negative.binomial(2))
-	print(summary(m13))
-	anova(m13)
-
-	m14 = glm.nb(wos ~ `pdf views` + `html views` , dat=subset.gt0)
-	print(summary(m14))
-	anova(m14)
-	
-	anova(m13, m14)
-
-m15 = lm((wos) ~ (`pdf views`) + (`html views`), dat=subset.gt0); summary(m15)
-
-
-
-
-dat = dat.eventcounts.merge.2008[,c(names(dat.events.perDoi), "daysSincePublished", "authorsCount")]
-dat = dat[,names(dat) %nin% c("doi")]
-names(dat) = substr(names(dat), 1, 3)
-attach(dat)
-
-
-library(fields)
-stats(dat)
-var(wos)
-
-stats(tr(dat))
-var(tr(wos))
-
-
-stats(log(1+wos))
-var(log(1+wos))
-
-stats(sqrt(1+wos))
-var(sqrt(1+wos))
-
-quartz()
-hist(wos, n=20)
-
-library(MASS)
-
-summary(glm(citations ~ ., data=as.data.frame(scores.1st)))
-
-
-m1 = glm.nb(wos ~ day+htm, data=dat)
-
-
-
-m1 = glm.nb(wos ~ ., data=dat)
-
-m1 = glm.nb(wos ~ day+htm, data=dat)
-
-
-
-m1 = glm(wos ~ ., data=dat, family="poisson")
-summary(m1)
-anova(m1)
-
-dat.trans = cbind(tr(dat[,names(dat) %nin% c("day")]), dat$day)
-m2 = glm(wos ~ ., data=dat.trans, family="poisson")
-summary(m2)
-anova(m2)
-
-m3 = glm(wos ~ bac+cit+del+htm+nat+pdf+Pos+Res+xml+day, data=dat[which(dat$day < 700),], family="poisson")
-summary(m3)
-
-m4 = glm(wos ~ bac+cit+del+htm+nat+Pos+Res+xml+day, data=dat[which(dat$day < 700),], family="poisson")
-summary(m4)
-
-small = dat[which(dat$day < 700),]
-heatmap(cor(dat[which(dat$day < 700),], method="spearman"))
-
-heatmap(cor(tr(dat[which(dat$day < 700),])))
-cor(tr(dat[which(dat$day < 700),]))
-
-m5 = Glm(wos ~ (bac+cit+del+htm+nat+pdf+Pos+Res+xml)*day, data=dat[which(dat$day < 11000),], family="poisson")
-summary(m5)
-
-dd = datadist(dat)
-options(datadist="dd")
-
-m6 = Glm(wos ~ (bac+cit+del+htm+nat+pdf+Pos+Res+xml)*rcs(day, 6), data=dat[which(dat$day < 1100),], family="poisson")
-m6
-summary(m6)
-anova(m6)
-plot(summary(m6))
-
-table(dat.eventcounts.merge.2008$articleType)
-dd = datadist(dat.eventcounts.merge.2008[13:35])
-options(datadist="dd")
-datause = dat.eventcounts.merge.2008[which(dat.eventcounts.merge.2008$articleType == "Research Article"),]
-summary(datause)
-
-
-m9 = Glm(wos ~ rcs(daysSincePublished,3) * (wikipediaCites +
-	authorsCount + f1000Factor + backtweetsCount +
-	deliciousCount + facebookShareCount  + facebookCommentCount  + 
-	mendeleyReadersCount + almBlogsCount + pdfDownloadsCount + xmlDownloadsCount + 
-	htmlDownloadsCount + almCiteULikeCount + plosCommentCount),
-	data=datause[which(datause$journal == "pone"),], family="poisson")
-m9
-summary(m9)
-anova(m9)
-plot(summary(m9))
-
-boxplot(wos ~ journal, dat=datause)
-
-m8 = Glm(wos ~ rcs(daysSincePublished,3) * (wikipediaCites +
-	authorsCount + f1000Factor + backtweetsCount +
-	deliciousCount + facebookShareCount  + facebookCommentCount  + 
-	mendeleyReadersCount + almBlogsCount  + xmlDownloadsCount + 
-	htmlDownloadsCount + almCiteULikeCount + plosCommentCount),
-	data=datause[which(datause$journal == "pone"),], family="poisson")
-m8
-summary(m8)
-anova(m8)
-plot(summary(m8))
-
-
-m8 = Glm(wos ~ rcs(daysSincePublished,3) * (
-	authorsCount + backtweetsCount +
-	deliciousCount + 
-	almBlogsCount  + xmlDownloadsCount + 
-	htmlDownloadsCount + almCiteULikeCount),
-	data=datause[which(datause$journal == "pone"),], family="poisson")
-m8
-summary(m8)
-anova(m8)
-plot(summary(m8))
-
-library(stringr)
-names(datause) = str_replace_all(names(datause), " ", ".")
-
-dd = datadist(datause[,c(2:11,13:15,34)])
-options(datadist = "dd")
-
-m8 = Glm(wos ~ rcs(daysSincePublished,3) * (
-	authorsCount + backtweets + citeulike + delicious +
-	html.views + native.comments + pdf.views + Postgenomic.via.Plos + 
-	Research.Blogging.via.Plos + xml.views),
-	data=datause[which(datause$journal == "pone"),], family="poisson")
-m8
-summary(m8)
-anova(m8)
-plot(summary(m8))
-
-m8 = glm.nb(wos ~ rcs(daysSincePublished,3) * (
-	authorsCount + backtweets + citeulike + delicious +
-	html.views + native.comments + pdf.views + Postgenomic.via.Plos + 
-	Research.Blogging.via.Plos + xml.views),
-	data=datause[which(datause$journal == "pone"),]) #, family="poisson")
-m8
-summary(m8)
-anova(m8)
-plot(summary(m8))
-
-m8 = glm.nb(wos ~ rcs(daysSincePublished,3) * (
-	authorsCount + backtweets + citeulike + delicious +
-	html.views + native.comments + pdf.views + Postgenomic.via.Plos + 
-	Research.Blogging.via.Plos + xml.views),
-	data=datause[which(datause$daysSincePublished < 800 & datause$journal == "pone"),]) #, family="poisson")
-m8
-summary(m8)
-anova(m8)
-plot(summary(m8))
-
-m9 = glm.nb(wos ~ rcs(daysSincePublished,3) * (
-	authorsCount + backtweets + citeulike + delicious +
-	html.views + native.comments + pdf.views + Postgenomic.via.Plos + 
-	Research.Blogging.via.Plos + xml.views),
-	data=datause[which(datause$daysSincePublished > 800 & datause$daysSincePublished < 900 & datause$journal == "pone"),]) #, family="poisson")
-m9
-summary(m9)
-anova(m9)
-plot(summary(m9))
-
-m10 = glm.nb(wos ~ rcs(daysSincePublished,3) * (
-	authorsCount + backtweets + citeulike + delicious +
-	html.views + native.comments + pdf.views + Postgenomic.via.Plos + 
-	Research.Blogging.via.Plos + xml.views),
-	data=datause[which(datause$daysSincePublished > 900 & datause$daysSincePublished < 1000 & datause$journal == "pone"),]) #, family="poisson")
-m10
-summary(m10)
-anova(m10)
-plot(summary(m10))
-
-m11 = glm.nb(wos ~ 	authorsCount  + citeulike +
-	html.views , #+ pdf.views,
-	data=datause[which(datause$daysSincePublished > 900 & datause$daysSincePublished < 1000 & datause$journal == "pone"),]) #, family="poisson")
-m11
-summary(m11)
-anova(m11)
-plot(summary(m11))
-
-m12 = Glm(wos ~ 	authorsCount  + citeulike +
-	html.views + pdf.views,
-	data=datause[which(datause$daysSincePublished > 900 & datause$daysSincePublished < 1000 & datause$journal == "pone"),], family=negative.binomial(1))
-m12
-summary(m12)
-anova(m12)
-plot(summary(m12))
-
-a = c()
-for (n in 6:10) {
-	b = cor(datause[which(datause$daysSincePublished > n*100 & datause$daysSincePublished < (n+1)*100 & datause$journal == "pone"),c(2:11,15,34)])[,"wos"]
-	a = cbind(a, b)
-}
-a	
+library(lmtest)
+waldtest(fit.full, fit.base, test="Chisq")
+anova(fit.full, fit.base)
