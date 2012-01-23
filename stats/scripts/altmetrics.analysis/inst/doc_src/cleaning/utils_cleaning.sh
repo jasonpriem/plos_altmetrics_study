e8101a410d341f8f742a880c8ec5185c30e636ea
Heather Piwowar <hpiwowar@gmail.com>
2012-01-22 20:18:46 -0800

    remove package structure

--- stats/scripts/altmetrics.analysis/inst/doc_src/cleaning/utils_cleaning.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### @export "docs clean_crawler_counts"
-
-R CMD Rd2txt  ../../../man/normalize_altmetrics.Rd
-
-### @export "other"
-
-#R --vanilla -e ""
--- stats/scripts/altmetrics.analysis/inst/doc_src/normalization/utils_normalization.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### @export "docs clean_crawler_counts"
-
-R CMD Rd2txt  ../../../man/clean_normalize.Rd
-
-### @export "other"
-
-#R --vanilla -e ""
